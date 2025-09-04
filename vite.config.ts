7a9d01d087d7c49da055566699aa86061c90374b
Stewart <stewart_3@hotmail.com>
2025-09-04 13:53:21 -0500

    Added config option to enable running of app on azure app service

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ja-avatars/",
+  // base: "/",  //use for App Service
+  base: "/ja-avatars/", //use for locally running app
   server: {
     watch: {
       usePolling: true
